f6499b45e73be301f9f3eba611d445fca1bb14f1
Thiago Honorato <thiagohonore@gmail.com>
2022-03-10 21:39:47 -0300

    init

---
+++ src/tests/sample.spec.ts
@@ -0,0 +1,13 @@
+describe('test', () => {
+  test('should work', () => {
+    expect(true).toEqual(true);
+  });
+
+  test('should work', () => {
+    expect(true).toEqual(true);
+  });
+
+  test('should work 2', () => {
+    expect(true).toEqual(true);
+  });
+})
